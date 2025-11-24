b3aeec9ba2fed7c511a96f99a1d71f4986f8a945
Sebastian <amsebastian12@gmail.com>
2025-11-24 13:47:16 -0500

    Added damage interaction to knight and mage

--- planechaser/enemies/scripts/GruntController.cs
+++ planechaser/enemies/scripts/GruntController.cs
@@ -72,10 +72,11 @@ public partial class gruntController : meleeEnemy
         }
     }
     //Enemy takes damage in their special ways and dies
-    public override void Hit(string weapon, float damage)
+    public override void Hit(int weaponPlane, float baseDamage)
     {
+        /*
         float damageFinal = 0;
-        switch (weapon)
+        switch (weaponPlane)
         {
             case "AK":
                 break;
@@ -86,12 +87,23 @@ public partial class gruntController : meleeEnemy
             default:
                 break;
         }
-        currentHealth -= damageFinal;
+        */
+        if(weaponPlane == gameMaster.currentDimension)
+            currentHealth -= baseDamage * 1.5f;
+        else
+            currentHealth -= baseDamage;
+        GD.Print("Took damage!");
+        
         if(currentHealth <= 0)
         {
             animationTree.Set("parameters/conditions/Death", true);
         } else {
             animationTree.Set("parameters/conditions/Hit", true);
         }
+    }
+
+    public void TestHit()
+    {
+        GD.Print("Took Damage");
     }
 }
