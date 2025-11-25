98ceff022f786934c6c322be2eaf3f2ff889b7b5
Sebastian <amsebastian12@gmail.com>
2025-11-25 10:10:29 -0500

    Fixed weapon viewport and fov issues

--- planechaser/player/scripts/ui/WeaponViewport.cs
+++ planechaser/player/scripts/ui/WeaponViewport.cs
@@ -3,13 +3,15 @@ using System;
 
 public partial class WeaponViewport : SubViewport
 {
-	private Vector2I ScreenSize;
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
-		// Basic script that automatically sets the subwiewport window 
-		// to match the window size of the game without needing to adjust it manually
-		ScreenSize = GetWindow().Size;
-		Size = ScreenSize;
+    {
+        UpdateSize();
+        GetWindow().SizeChanged += UpdateSize;
+    }
+
+    private void UpdateSize()
+    {
+        Vector2I size = (Vector2I)GetViewport().GetVisibleRect().Size;
+        Size = size;
     }
 }
