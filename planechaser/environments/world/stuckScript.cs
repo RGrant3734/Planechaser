a7b404f3d030832e558518beda6a9465f6cb38ab
Robert Grant <112195519+RGrant3734@users.noreply.github.com>
2025-12-29 15:18:53 -0500

    Fixed stuck under ramps

---
+++ planechaser/environments/world/stuckScript.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class stuckScript : Area3D
+{
+    [Export]
+    public StaticBody3D mainObject;
+    private GameMaster _GameMaster;
+
+    public override void _Ready()
+    {
+        _GameMaster = GetNode<GameMaster>("/root/GameMaster");
+    }
+
+    public override void _Process(double delta)
+    {
+        if(GetParent<EnvironmentSwapper>().HiddenPlane != _GameMaster.currentDimension)
+        {
+            foreach (var body in GetOverlappingBodies())
+            {
+                GD.Print(body);
+                if(body.IsInGroup("Player") || body.IsInGroup("Enemy"))
+                {
+                    body.GlobalPosition = new Vector3(body.GlobalPosition.X, body.GlobalPosition.Y + Scale.Y, body.GlobalPosition.Z);
+                }
+            }
+        }
+    }
+}
